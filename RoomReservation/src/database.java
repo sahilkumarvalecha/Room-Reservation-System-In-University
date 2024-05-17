1d0e0b0c12ff0c92faa8f2a4cbd2cf20568d47d6
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2024-05-17 17:51:27 +0500

    login work

--- RoomReservation/src/database.java
+++ RoomReservation/src/database.java
@@ -1,3 +1,10 @@
+
+import com.sun.jdi.connect.spi.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,7 +14,22 @@
  *
  * @author Siloo
  */
-public class database {
+public class Database {
+    public static Connection mycon(){
+        Connection con = null;gi
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/roomreservationsysteminuniversity","root","");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        k
+        return con;
+    }
+
+  
     
     
 }
